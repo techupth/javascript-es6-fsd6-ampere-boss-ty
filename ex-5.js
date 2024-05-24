188a8f868ba3d8beb4ee75f1647fe879dbaaa163
paphonwat <paphonwat.w@gmail.com>
2024-05-24 13:05:46 +0700

    Feat:add ex1-ex7

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,8 @@
-function sum() {
+function sum(...input) {
   // Start coding here !
+    console.log(input)
+    return input.reduce((acc,curr) => acc + curr,0)
+    
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
