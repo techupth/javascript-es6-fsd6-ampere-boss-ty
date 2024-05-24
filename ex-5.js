d1e60c9114e5eb9f920d1b297d69a54bc836a903
Ampare <visarut.thk@gmail.com>
2024-05-24 13:51:08 +0700

    Assignment:Js-ES6

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,9 @@
-function sum() {
+function sum(...num) {
   // Start coding here !
+  let result = num.reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
+  return result;
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
