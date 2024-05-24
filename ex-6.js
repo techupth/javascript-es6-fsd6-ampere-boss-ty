6122794f1704e81bda48a602941cd1ed6db7ea65
TiTY <tee25450@gmail.com>
2024-05-24 13:54:41 +0700

    ex1-7(done)

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,8 @@
 let height = undefined;
 let result;
+function check(height) {
+  return height ?? "Height is not defined";
+}
+result = check(height);
 
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
